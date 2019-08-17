1338aad337d61e2fdad3afad2279739f53defe4a
Douglas Mateus Soares Cândido da Silva <douglas@douglas-debian.com.br>
2019-08-17 00:35:07 -0300

    Atualização.

---
+++ testes.py
@@ -0,0 +1,16 @@
+import connection
+import validacao
+import getpass
+
+running = connection.connection(
+    'root', 'douglas', 'localhost', 'bd_sig_banking')
+
+# Cadastro de alguns bancos no sistema:
+
+
+running.add_bank('00.360.305/0001-04', 'Caixa Econômica Federal', 'cef@cef.com.br')
+running.add_bank('00.000.000/0001-91', 'Banco do Brasil S/A', 'bb@bb.com.br')
+running.add_bank('60.872.504/0001-23', 'Itaú Unibanco Holding S/A', 'itau@itau.com.br')
+running.add_bank('60.746.948.0001-12', 'Banco Bradesco S/A', 'bradesco@bradesco.com.br')
+
+running.connection_close()
